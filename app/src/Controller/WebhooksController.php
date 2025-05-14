74b30fc0f2943735fc564eb3e0ded07b9d4e4cb3
birne <birneos@gmx.net>
2025-05-14 13:26:07 +0200

    Webhookcontroller serialize Daten aufs DTO

--- app/src/Controller/WebhooksController.php
+++ app/src/Controller/WebhooksController.php
@@ -2,29 +2,44 @@
 
 declare(strict_types=1);
 
-
 namespace App\Controller;
 
-use App\CDP\Http\CdpClient;
+use App\DTO\Webhook;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class WebhooksController extends AbstractController
 {
-    public function __construct(
-        private CdpClient $cdpClient,
-    ) {
-    }
+  public function __construct(
+    private readonly SerializerInterface $serializer,
 
-    #[Route(path: '/webhook', name: 'webhook', methods: ['POST'])]
-    public function handleWebhook(Request $request): Response
-    {
-        // Handle the webhook request
-        // You can use $this->cdpClient to send data to the CDP
+  ) {}
 
-        return new Response(status: 204);
+  #[Route(path: '/webhook', name: 'webhook', methods: ['POST'])]
+  public function __invoke(Request $request): Response
+  {
+
+    // Handle the webhook request
+    // You can use $this->cdpClient to send data to the CDP
+    try {
+      $webhook = $this->serializer->deserialize(
+        $request->getContent(),
+        
+        Webhook::class,
+        'json'
+      );
+
+      $webhook->setRawPayload(json_decode($request->getContent(), true));
+
+      dd($webhook);
+
+      return new Response(status: 204);
+    } catch (\Throwable $exception) {
+
+      throw $exception;
     }
+  }
 }
-
