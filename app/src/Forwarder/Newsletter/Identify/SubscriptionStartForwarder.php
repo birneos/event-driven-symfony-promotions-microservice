4115a4d746596d59fd92da4c2868762f78ab1239
birne <birneos@gmx.net>
2025-05-22 08:33:51 +0200

    Fix: Ordner korrekt verschoben und Namespace angepasst

--- app/src/Forwarder/Newsletter/Identify/SubscriptionStartForwarder.php
+++ app/src/Forwarder/Newsletter/Identify/SubscriptionStartForwarder.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Forwarder\Identify;
+namespace App\Forwarder\Newsletter\Identify;
 
 use App\CDP\Analytics\Model\ModelValidator;
 use App\CDP\Analytics\Model\Subscription\Identify\IdentifyModel;
